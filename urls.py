ebbfab6bd208b152a03e7da178af8d7ae372f1a6
Luis Capote <lcapote@opensistemas.com>
2023-04-20 08:33:59 -0500

    clear unnecesary

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
-
-from .views import messengerapp_view
-
-urlpatterns = [
-    url(r"^/$", messengerapp_view , name="messengerapp_view")
-]
-
